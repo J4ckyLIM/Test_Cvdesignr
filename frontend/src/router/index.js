37ce7f8fc8dc7354b00cfb408739268b6a8851a0
JackyLIM <jacky.lim@edu.itescia.fr>
2020-08-22 11:19:42 +0200

    Feat: Add new route to router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: () => import('@/views/Home')
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: () => import('@/views/Login')
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: () => import('@/views/Register')
     }
   ]
 })
