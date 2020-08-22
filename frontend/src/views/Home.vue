74ec7129d92bde74dc736f65750906e8e9ea0e4b
JackyLIM <jacky.lim@edu.itescia.fr>
2020-08-22 12:15:59 +0200

    Feat: front for homepage

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <div class="container-fluid mt-5">
+      <div class="row">
+        <div class="col">Sidebar 1</div>
+        <div class="col-5">
+          <div class="card mt-2">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+          <div class="card mt-2">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">Sidebar 2</div>
+      </div>
+    </div>
   </div>
 </template>
 
