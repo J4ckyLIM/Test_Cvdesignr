d9df61de5c122f3b2516bc486802b9c83f638b62
JackyLIM <jacky.lim@edu.itescia.fr>
2020-08-22 11:44:46 +0200

    Feat: import navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <MainNavBar />
     <router-view/>
   </div>
 </template>
 
 <script>
+import MainNavBar from '@/components/navbar/MainNavBar'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    MainNavBar
+  }
 }
 </script>
 
@@ -18,6 +23,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
 }
 </style>
