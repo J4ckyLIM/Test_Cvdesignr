2ae84da9d89329465b888bb2747a9ed3146e206c
JackyLIM <jacky.lim@edu.itescia.fr>
2020-08-22 15:30:59 +0200

    Feat: Add user route import

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,31 @@
-const express = require("express");
-const app = express();
-const config = require("./config/config");
-const bodyParser = require("body-parser");
+const express = require('express')
+const app = express()
+const config = require('./config/config')
+const bodyParser = require('body-parser')
 
 // Importing all routers
-const offerRouter = require("./routes/offers");
+const offerRouter = require('./routes/offers')
+const userRouter = require('./routes/users')
 
 const cors = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "*");
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', '*')
+  res.header('Access-Control-Allow-Headers', '*')
   next();
 };
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors);
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors)
 
-app.get("/", function (req, res) {
-  res.send("Welcome to App");
+app.get('/', function (req, res) {
+  res.send('Welcome to App')
 });
 
 // Defining API Routes
-app.use("/offer", offerRouter);
+app.use('/offer', offerRouter)
+app.use('/user', userRouter)
 
 app.listen(config.port, function () {
-  console.log("App running on localhost:%s", config.port);
+  console.log('App running on localhost:%s', config.port)
 });
