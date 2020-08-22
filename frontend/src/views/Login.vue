b272be2899cc2cd63adb76b8c103ad5de152cc60
JackyLIM <jacky.lim@edu.itescia.fr>
2020-08-22 12:08:45 +0200

    Feat: Front for login page

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,6 +1,26 @@
 <template>
-  <div class="hello">
+  <div class="mt-4">
     <h1>{{ msg }}</h1>
+    <div class="container w-25 mt-5">
+      <div class="card text-center">
+        <div class="card-header">
+          Connexion
+        </div>
+        <div class="card-body">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Mot de passe</label>
+              <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <button type="submit" class="btn btn-primary">Je me connecte</button>
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,7 +29,7 @@ export default {
   name: 'Login',
   data () {
     return {
-      msg: 'Welcome to Login'
+      msg: 'Bienvenue, veuillez vous connecter pour utiliser l\'application'
     }
   }
 }
